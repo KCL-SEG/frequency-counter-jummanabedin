b7a1ef2ea370cbf15822e12a8296dfced9a754a0
jummanabedin <91836600+jummanabedin@users.noreply.github.com>
2022-10-22 14:01:32 +0100

    update freq func

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
+    string_list = [str(i) for i in items]
     frequencies = {}
-    # Your code goes here
+    for item in string_list:
+        if item in frequencies:
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
     return frequencies
+
+
